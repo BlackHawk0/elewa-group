1fce9991b610eca900ed4f7f55ce775f08cfb802
Daniel <danielngoys@outlook.com>
2023-02-07 14:00:16 +0300

    🚩: removed header

--- libs/elements/layouts/hero/src/lib/hero/hero.component.ts
+++ libs/elements/layouts/hero/src/lib/hero/hero.component.ts
@@ -9,5 +9,5 @@ export class HeroComponent {
   @Input() subtitle = 'HOW TO RECOGNIZE AND ELEWA MEMBER';
   @Input() title = 'Dependable, Creative, Supportive, Open Minded and Fun*';
   @Input() footerText = '*SERIOUS WHEN IT MATTERS';
-  @Input() backgroundImage = 'https://images.unsplash.com/photo-1675432980667-95da207814a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80';
+  @Input() backgroundImage = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg';
 }
